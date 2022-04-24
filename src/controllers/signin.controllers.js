80898367d1edeee7a9605f642a3c4c0ead7dc306
carlos bravo <carloslife2014@outlook.com>
2022-04-24 03:15:32 -0500

    formilario validado

--- src/controllers/signin.controllers.js
+++ src/controllers/signin.controllers.js
@@ -4,34 +4,34 @@ import User from "../models/User";
 
 //:
 export const renderSignin = async (req, res) => {
-  res.render("signin");
+	res.render("signin");
 };
 //:sign in user
 export const signinUser = async (req, res) => {
-  res.send("helloworld");
+	res.send("helloworld");
 }
 export const createUser = async (req, res) => {
-  const { name, email, password, confirmpassword, matricula } = req.body;
-  const errors = [];
-  //-validar si el usuario ya existe
-
-  //-validacion de campos vacios
-  if (name.length <= 0 || email <= 0 || password <= 0 || confirmpassword <= 0 || matricula <= 0) {
-    errors.push({ text: "Complete the empty fields" });
-  }
-  if (password != confirmpassword) {
-    errors.push({ text: "Password do not match" });
-  }
-  if (password.length < 7) {
-    errors.push({ text: "Password must be at least 7 characters" });
-  }
-
-  if (errors.length > 0) {
-    res.json('/', { errors, name, email, password, confirmpassword, matricula });
-  }
-  if (errors.length == 0) {
-
-    res.redirect("/index")
-  }
+	const { name, email, password, confirmpassword, matricula } = req.body;
+	const errors = [];
+	//-validar si el usuario ya existe
+		
+	//-validacion de campos vacios
+	if (name <= 0 || email <= 0 || password <= 0 || confirmpassword <= 0 || matricula <= 0) {
+		errors.push("Complete the empty fields");
+	}
+	if (password.length > 0) {
+		if (password != confirmpassword) {
+			errors.push("Password do not match");
+		}
+	}
+	if (password.length > 0 && password.length < 7) {
+		errors.push("Password must be at least 7 characters");
+	}
+	if (errors.length > 0) {
+		res.json({ errors, name, email, password, confirmpassword, matricula });
+	}
+	if (errors.length == 0) {
+		res.json({errors})
+	}
 }
 //
--- src/public/scripts/signin.scripts.js
+++ src/public/scripts/signin.scripts.js
@@ -9,7 +9,50 @@ function mcSignin() {
     $("#collapseSignin").show();
     $("#collapseCreate").hide();
 }
-function mcCreate(){
+function mcCreate() {
     $("#collapseSignin").hide();
     $("#collapseCreate").show();
 }
+
+var btnCreateUser = document.querySelectorAll('#btnCreateUser')
+$(btnCreateUser).click(function (e) {
+    e.preventDefault();
+    //seleccionar los inputs y evaluarlos
+    var inputName = $('#createname').val();
+    var inputEmail = $('#createemail').val();
+    var inputPaswword = $('#createpassword').val();
+    var inputConfirmpassword = $('#confirmpassword').val();
+    var inputMatricula = $('#creatematricula').val();
+    $.post('/create/user', { name: inputName, email: inputEmail, password: inputPaswword, confirmpassword: inputConfirmpassword, matricula: inputMatricula }).done(data => {
+        console.log(data.errors);
+        if(data.errors>0){
+            for (var i = 0; i < data.errors.length; i++) {
+                var divName='div'+i;
+                var divName = document.createElement('div');
+                divName.setAttribute('id', 'divErrors')
+                divName.setAttribute('role', 'alert')
+                divName.setAttribute('class', 'alert alert-danger d-flex align-items-center justify-content-between')
+                divName.innerText = data.errors[i];
+                var btnClose="btn"+i;
+                var btnClose = document.createElement('button')
+                btnClose.setAttribute('type', 'button')
+                btnClose.setAttribute('class', 'btn-close')
+            btnClose.setAttribute('data-bs-dismiss', 'alert')
+            btnClose.setAttribute('aria-label', 'Close')
+            divName.appendChild(btnClose)
+            const conterrors = document.getElementById('conterrors')
+            conterrors.append(divName)
+            }
+        }else{
+            window.location.href='http://localhost:3000/index'
+        }
+        
+        
+       
+        
+       
+        
+        //type="button" class="btn-close " data-bs-dismiss="alert" aria-label="Close"
+
+    })
+})
